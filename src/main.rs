ca79641611821f4932a6f4c17fe5afabb1772f73
Alexander Davidson Bryan <alxdb@gmail.com>
2019-02-09 12:55:15 +0000

    Base

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ use aesthetics::{
     window_utils,
 };
 use nalgebra_glm as glm;
-use specs::{world::Builder, RunNow, World};
+use specs::{world::Builder, DispatcherBuilder, RunNow, World};
 
-use std::time::Instant;
+// use std::time::Instant;
 
 fn init_display() -> (glium::glutin::EventsLoop, glium::Display) {
     let window_builder = glium::glutin::WindowBuilder::new()
-        // .with_fullscreen(Some(window_utils::get_primary_monitor()))
+        .with_fullscreen(Some(window_utils::get_primary_monitor()))
         .with_title("Hello world");
     let context_builder = glium::glutin::ContextBuilder::new()
         .with_multisampling(8)
@@ -35,35 +35,37 @@ fn main() {
         // })
         .with(renderers::camera::Camera::Persp {
             fov: 45.0,
-            aspect: 1.0,
+            aspect: 1920.0 / 1080.0,
         })
         .with(transform::Transform {
             pos: glm::vec3(0.0, 0.0, 3.0),
             // rot: glm::quat_look_at(&glm::vec3(0.0, 0.5, 0.0), &glm::vec3(0.0, 1.0, 0.0)),
-            rot: glm::quat_angle_axis(45.0, &glm::vec3(1.0, 0.0, 0.0)),
-            // rot: glm::Quat::identity(),
+            // rot: glm::quat_angle_axis(45.0, &glm::vec3(1.0, 0.0, 0.0)),
+            rot: glm::Quat::identity(),
         })
         .build();
 
     mesh_renderer.set_main_camera(camera);
 
-    let cube = world
-        .create_entity()
-        .with(mesh::cube((1.0, 1.0, 1.0)))
-        .with(transform::Transform {
-            pos: glm::vec3(0.0, 0.5, 0.0),
-            rot: glm::quat_angle_axis(-45.0, &glm::vec3(1.0, 0.0, 0.0)),
-        })
-        .build();
-
-    // let sphere = world
+    // let _cube = world
     //     .create_entity()
-    //     .with(mesh::sphere(1.0, 13))
-    //     .with(transform::Transform::new(glm::Vec3::new(0.0, 0.0, 0.0)))
+    //     .with(mesh::cube((1.0, 1.0, 1.0)))
+    //     .with(transform::Transform {
+    //         pos: glm::vec3(0.0, 0.5, 0.0),
+    //         rot: glm::quat_angle_axis(-45.0, &glm::vec3(1.0, 0.0, 0.0)),
+    //     })
     //     .build();
 
+    let _sphere = world
+        .create_entity()
+        .with(mesh::sphere(1.0, 13))
+        .with(transform::Transform::new(glm::Vec3::new(0.0, 0.0, 0.0)))
+        .build();
+
+    let mut dispatcher = DispatcherBuilder::new().with_thread_local(renderers::basic::Renderer);
+
     'main: loop {
-        let start = Instant::now();
+        // let start = Instant::now();
         // Input Handle
         for ev in window_utils::get_events(&mut ev_loop).iter() {
             match ev {
@@ -75,8 +77,11 @@ fn main() {
             }
         }
 
+        // rotate object
+
         // Rendering
         mesh_renderer.run_now(&world.res);
         // println!("dur: {:#?}", Instant::now() - start);
     }
+    println!("exited cleanly")
 }
